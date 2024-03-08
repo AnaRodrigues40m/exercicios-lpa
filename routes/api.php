f1b5f600fcc4a9edabd8032fddcedb07fdf103af
AnaRodrigues40m <silvaprtt40@gmail.com>
2024-03-08 10:23:53 -0300

    exercicio 2

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,13 @@ Route::get('receber/nome', function (Request $request){
     $nome = $request -> input('name');
     return $nome;
     });
+
+    Route::get('calcular/media', function(Request $request){
+        
+        $primeiraEntrada = $request -> input('primeiraNote');
+        $segundaEntrada = $request -> input('segundaNota');
+    
+    
+        $media = ($primeiraEntrada + $segundaEntrada)/2;
+        return $media;
+    });
